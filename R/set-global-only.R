4284d60e5f858f674bd07ba03bff54c672c5fe65
huhuaping <huhuaping01@nwsuaf.edu.cn>
2021-11-09 16:43:38 +0800

    webshot DT htmlwidgets

--- R/set-global-only.R
+++ R/set-global-only.R
@@ -1,20 +1,20 @@
 
 # global options for R
 options(
-  htmltools.dir.version = FALSE, 
   formatR.indent = 2, width = 55, 
   digits = 2,scipen=999, tinytex.verbose = TRUE,
   knitr.kable.NA = '',
   # html widget for xaringan slide
-  #htmltools.dir.version = FALSE, 
-  #htmltools.preserve.raw = FALSE,
+  htmltools.dir.version = FALSE, 
+  #htmltools.preserve.raw = T,
+  #widgetframe_widgets_dir = 'widgets',
   echo=FALSE, warning=FALSE, message=FALSE,comment="")
 
 # global options for knitr
 knitr::opts_chunk$set(fig.align='center',echo = FALSE,
                       message = FALSE,comment="",
                       # html widget for docx,
-                      #screenshot.force = knitr::pandoc_to("docx"),
+                      screenshot.force = knitr::pandoc_to("docx"),
                       #widgetframe_widgets_dir = 'widgets',
                       fig.width=11, fig.height=5.5) # Places figures on their own pages
 
