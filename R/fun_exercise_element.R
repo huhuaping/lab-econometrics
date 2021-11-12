d1574aa045cce593d3c42f10de8b111512c5b465
huhuaping <huhuaping01@nwsuaf.edu.cn>
2021-11-12 10:00:57 +0800

    lab02 consumption exercise finishd

--- R/fun_exercise_element.R
+++ R/fun_exercise_element.R
@@ -11,12 +11,14 @@
 #'
 #' @examples
 #' 
-name_file <- function(num = 0, id_demo ="2019000001", 
+name_file <- function(num = 0, type,
+                      id_demo ="2019000001", 
                       name_demo = "张三", ext = "docx"){
   name_file <- paste0(
     "lab",
     stringr::str_pad(num,2,'left','0'
     ),
+    "_",type,
     "_",name_demo,
     "_",id_demo,
     ".", ext,
